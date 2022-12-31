0f373e4e16e054c2e3a97a5b9fd7550a2129ec17
Alex <rubanovv.alex@gmail.com>
2022-12-31 15:16:22 +0100

    added localStorage

--- script.js
+++ script.js
@@ -7,8 +7,54 @@ const api = {
 };
 
 const locationInput = document.querySelector('.search-box');
+const currentDate = new Date();
 
 
+function dateBuilder(data) {
+    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday'];
+
+    const day = days[data.getDay()];
+    const date = data.getDate();
+    const month = months[data.getMonth()];
+    const year = data.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+}
+
+const locationData = {
+    city: 'London',
+    country: 'GB',
+    time: dateBuilder(currentDate),
+    temp: 15,
+    descr: 'Sunny',
+    mintemp: 13,
+    maxtemp: 16
+};
+
+const saveLocationData = () => {
+    localStorage.setItem('locationData', JSON.stringify(locationData));
+};
+
+const render = () => {
+    const parentNode = document.querySelector('main');
+    let data = localStorage.getItem('locationData');
+    data = JSON.parse(data) || locationData;
+
+    parentNode.innerHTML = `
+    <section class="location">
+        <div class="location-city">${data.city}, ${data.country}</div>
+        <div class="location-date">${data.time}</div>
+    </section>
+    <section class="temperature">
+        <div class="temperature-current">${data.temp}<span>&#176c</span></div>
+        <div class="temperature-descr">${data.descr}</div>
+        <div class="temperature min-max">${data.mintemp}&#176c / ${data.maxtemp}&#176c</div>
+    </section>    
+    `;
+};
+
+render();
 
 locationInput.addEventListener('keydown', (e) => {
     if (e.keyCode == 13) {
@@ -17,14 +63,12 @@ locationInput.addEventListener('keydown', (e) => {
     }
 });
 
-function setLocation(input) {
+const setLocation = (input) => {
     const query = input.value;
     input.value = '';
 
-    console.log(query);
-    return query;
-    
-}
+    return query; 
+};
 
 const getResults = async (location) => {
     await fetch(`${api.base}weather?q=${location}&appid=${api.key}&units=${api.units}`)
@@ -35,29 +79,15 @@ const getResults = async (location) => {
 };
 
 
-function displayResults(data) {
-    const city = document.querySelector('.location-city');
-    const temperature = document.querySelector('.temperature-current');
-    const description = document.querySelector('.temperature-descr');
-    const minmaxTemperature = document.querySelector('.temperature .min-max');
-    const date = document.querySelector('.location-date');
-    const now = new Date();
+const displayResults = (data) => {
+    locationData.city = data.name;
+    locationData.country = data.sys.country;
+    locationData.time = dateBuilder(currentDate);
+    locationData.temp = Math.round(data.main.temp);
+    locationData.descr = data.weather[0].main;
+    locationData.mintemp = Math.round(data.main.temp_min);
+    locationData.maxtemp = Math.round(data.main.temp_max);
 
-    city.innerHTML = `${data.name}, ${data.sys.country}`;
-    temperature.innerHTML = `${Math.round(data.main.temp)}&#176c`;
-    date.textContent = dateBuilder(now);
-    description.innerHTML = `${data.weather[0].main}`;
-    minmaxTemperature.innerHTML = `${Math.round(data.main.temp_min)}&#176c / ${Math.round(data.main.temp_max)}&#176c`;
-}
-
-function dateBuilder(data) {
-    const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Nobember', 'December'];
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thurday', 'Friday', 'Saturday'];
-
-    const day = days[data.getDay()];
-    const date = data.getDate();
-    const month = months[data.getMonth()];
-    const year = data.getFullYear();
-
-    return `${day} ${date} ${month} ${year}`;
-}
+    saveLocationData();
+    render();
+};
